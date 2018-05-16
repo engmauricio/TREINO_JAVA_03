6ec08a84d9ec26a45bc0fb84f18534bdabdad0de
mauricio10am@gmail.com <mauricio10am@gmail.com>
2018-05-15 23:29:31 -0300

    utilizando o conceito de orientação a objetos

---
+++ src/treino_java_003/TREINO_JAVA_003.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package treino_java_003;
+
+/**
+ *
+ * @author EngMauricio
+ */
+public class TREINO_JAVA_003 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    
+            writerTela mywriterTela = new writerTela();
+                
+                mywriterTela.displayMessage();
+    }
+    
+}
---
+++ src/treino_java_003/writerTela.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package treino_java_003;
+
+/**
+ *
+ * @author EngMauricio
+ */
+public class writerTela {
+    
+    public void displayMessage(){
+    
+        System.out.println(" programar em java é muito bom ");
+}
+}
